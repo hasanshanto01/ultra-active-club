4c30c99d2dc5496b0947e6767b34bfdb20eedb30
hasanshanto01 <hasanshanto01@gmail.com>
2022-10-02 02:17:15 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2 className='font-bold text-3xl'>hello react</h2>
-      <button className="btn btn-primary">Button</button>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+
+const Header = () => {
+    return (
+        <div className="navbar bg-accent">
+            <div className="flex-1 ml-10">
+                <img src={logo} alt="" className='w-10' />
+                <a className="text-3xl font-bold ml-3">Fitness Freaks</a>
+            </div>
+            <div className="flex-none mr-10">
+                <ul className="menu menu-horizontal p-0 text-xl font-semibold">
+                    <li><a href='/exercise'>Exercise</a></li>
+                    <li><a href='/about'>About</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
